b1b74f4c6d34aac4b6f16c7bc4ad5ecc1dbc38fa
lovefree <kmkd87@gmail.com>
2022-12-21 19:20:59 +0900

    로그 찍어보기

--- app/src/main/java/com/keyflow/kotlinbasic01_lovefree/MainActivity.kt
+++ app/src/main/java/com/keyflow/kotlinbasic01_lovefree/MainActivity.kt
@@ -9,6 +9,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        smallBtn
+        clickBtn.setOnClickListener {
+
+            // 코드에 메모 남기기 - 주석
+//            Ctrl + / : 커서가 있는 줄을 주석 처리
+//            이 {} 안에 있는 코드만 버튼이 눌렸을 때 실행됨.
+            Log.d("메인화면로그", "클릭용버튼눌림")
+
     }
 }
